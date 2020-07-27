5f8e440de7471651e6699891fb25210014fe3a84
Allen Liu <kamyu104@gmail.com>
2020-07-28 05:07:36 +0800

    update

--- Qualification Round/timber.py
+++ Qualification Round/timber.py
@@ -7,38 +7,19 @@
 # Space: O(N)
 #
 
-def expand(P, p_h, max_length, direction):
-    for p, h in direction(P): 
-        if p in max_length:
-            continue
-        l = 0
-        while True:
-            next_p, h = p_h[p]
-            if next_p in max_length:
-                break
-            l += h
-            max_length[next_p] = l
-            if next_p not in p_h:
-                break
-            p = next_p
+from collections import defaultdict
 
 def timber():
     N = input()
     P = [map(int, raw_input().strip().split()) for _ in xrange(N)]
     P.sort()
-    left, right = {p:(p-h, h) for p, h in P}, {p:(p+h, h) for p, h in P}
-    left_max_length, right_max_length = {}, {}
-    expand(P, left, left_max_length, reversed)
-    expand(P, right, right_max_length, lambda x:x)
+    lookup = defaultdict(lambda:defaultdict(int))
     result = 0
-    for p, l in left_max_length.iteritems():
-        result = max(result, l)
-        if p in right_max_length:
-            result = max(result, l+right_max_length[p])
-    for p, l in right_max_length.iteritems():
-        result = max(result, l)
-        if p in left_max_length:
-            result = max(result, l+left_max_length[p])
+    for d, direction in ((1, lambda x:x), (-1, reversed)):
+        for p, l in direction(P):
+            lookup[d][p+d*l] = max(lookup[d][p+d*l], lookup[d][p]+l)
+        for p, l in lookup[d].iteritems():
+            result = max(result, l+lookup[-d][p])
     return result
 
 for case in xrange(input()):

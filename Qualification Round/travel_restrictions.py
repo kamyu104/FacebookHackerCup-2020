230ebcb01f4ab36aed94b3e932c32526dad69396
Allen Liu <kamyu104@gmail.com>
2020-07-31 23:09:41 +0800

    update

--- Qualification Round/travel_restrictions.py
+++ Qualification Round/travel_restrictions.py
@@ -11,12 +11,14 @@ def travel_restrictions():
     N = input()
     I, O = [raw_input().strip() for _ in xrange(2)]
     result = []
-    for start in xrange(N):
-        left = right = start
-        while left-1 >= 0 and O[left] == I[left-1] == 'Y':
-            left -= 1
-        while right+1 < N and O[right] == I[right+1] == 'Y':
-            right += 1
+    left, right = -1, -1
+    for i in xrange(N):
+        if not (i-1 >= 0 and O[i] == I[i-1] == 'Y'):
+            left = i
+        if right < i:
+            right = i
+            while right+1 < N and O[right] == I[right+1] == 'Y':
+                right += 1
         result.append('N'*left + 'Y'*(right-left+1) + 'N'*(N-1-right))
     return "\n" + "\n".join(result)
 
